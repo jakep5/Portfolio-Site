8190eb0654433c93e6e5f90d44532aab5aba566b
jakep5 <jakepagel1@yahoo.com>
2019-12-05 15:25:00 -0600

    add nav bar media query

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,78 @@
 import React from 'react'
 import styles from './styles.module.css'
+import jQuery from 'jquery'
 
-export default function NavBar() {
-    return (
-        <div className={styles.nav}>
-          <div className={styles.navHeader}>
-            <div className={styles.navTitle}>
+export default class NavBar extends React.Component{
+
+    componentDidMount() {
+        jQuery(document).ready(function($) {
+          $("a#bioLink").on("click", function(e) {
+            e.preventDefault();
+            $("body, html").animate({ 
+              scrollTop: $( $(this).attr('href') ).offset().top 
+            }, 600);
+          });
+        });
+
+        jQuery(document).ready(function($) {
+          $("a#skillsLink").on("click", function(e) {
+            e.preventDefault();
+            $("body, html").animate({ 
+              scrollTop: $( $(this).attr('href') ).offset().top 
+            }, 600);
+          });
+        });
+
+        jQuery(document).ready(function($) {
+          $("a#projectsLink").on("click", function(e) {
+            e.preventDefault();
+            $("body, html").animate({ 
+              scrollTop: $( $(this).attr('href') ).offset().top 
+            }, 600);
+          });
+        });
+
+        jQuery(document).ready(function($) {
+          $("a#contactInfoLink").on("click", function(e) {
+            e.preventDefault();
+            $("body, html").animate({ 
+              scrollTop: $( $(this).attr('href') ).offset().top 
+            }, 600);
+          });
+        });
+
+
+
+        
+    }
+
+    render() {
+
+        return (
+            <div className={styles.nav}>
+              <div className={styles.navHeader}>
+                <div className={styles.navTitle}>
+                </div>
+              </div>
+              <div className={styles.navBtn}>
+                <label for="navCheck">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </label>
+              </div>
+
+              <div className={styles.navLinks}>
+                <a href="#bio" target="_blank" id="bioLink">Bio</a>
+                <a href="#skills" target="_blank" id="skillsLink">Skills</a>
+                <a href="#projects" target="_blank"id="projectsLink">Projects</a>
+                <a href="#contactInfo" target="_blank" id="contactInfoLink">Contact info</a>
+              </div>
             </div>
-          </div>
-          <div className={styles.navBtn}>
-            <label for="navCheck">
-              <span></span>
-              <span></span>
-              <span></span>
-            </label>
-          </div>
+        )
 
-          <div className={styles.navLinks}>
-            <a href="http://stackoverflow.com/users/4084003/" target="_blank">Bio</a>
-            <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">Skills</a>
-            <a href="https://codepen.io/jo_Geek/" target="_blank">Projects</a>
-            <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">Contact info</a>
-          </div>
-        </div>
-    )
+    }
+
+
+    
 }
