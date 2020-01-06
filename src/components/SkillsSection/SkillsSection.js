efb8c0173bbb3345b739cfe7f9eafc27abc187b3
jakep5 <jakepagel1@yahoo.com>
2020-01-06 11:26:42 -0600

    add styles for smaller mobile screens

--- src/components/SkillsSection/SkillsSection.js
+++ src/components/SkillsSection/SkillsSection.js
@@ -24,6 +24,8 @@ export default class SkillsSection extends Component {
                     <img className={styles.img1} src="https://user-images.githubusercontent.com/53489910/71267334-38718980-2342-11ea-83cc-313c0e6e8a25.png" />
                     <img className={styles.img2} src="https://user-images.githubusercontent.com/53489910/71267340-3a3b4d00-2342-11ea-99d1-696d108c0cde.png" />
                     <img className={styles.img3} src="https://user-images.githubusercontent.com/53489910/71267345-3ad3e380-2342-11ea-925d-79ecab828652.png" />
+
+                    <p className={styles.skillsText}>*10 corresponds to my highest level of mastery</p>
                     
                 </div>
                 <div role="buttonHolder3" className={styles.buttonHolder3}>
