fb86ddc4e8b18b09ccfc9d31bd8a8f5d0e14fc92
jakep5 <jakepagel1@yahoo.com>
2020-05-27 15:30:01 -0500

    update skill images

--- src/components/SkillsSection/SkillsSection.js
+++ src/components/SkillsSection/SkillsSection.js
@@ -22,9 +22,9 @@ export default class SkillsSection extends Component {
                     <h1 className={styles.skillsHeading}>Skills</h1>
 
                     <div className={styles.imageHolder}>
-                        <img alt="Skills display 1" className={styles.img1} src="https://user-images.githubusercontent.com/53489910/71267334-38718980-2342-11ea-83cc-313c0e6e8a25.png" />
-                        <img alt="Skills display 2" className={styles.img2} src="https://user-images.githubusercontent.com/53489910/71267340-3a3b4d00-2342-11ea-99d1-696d108c0cde.png" />
-                        <img alt="Skills display 3" className={styles.img3} src="https://user-images.githubusercontent.com/53489910/71267345-3ad3e380-2342-11ea-925d-79ecab828652.png" />
+                        <img alt="Skills display 1" className={styles.img1} src="https://user-images.githubusercontent.com/53489910/83068661-45e81980-a02e-11ea-8166-f9fe643a1723.png" />
+                        <img alt="Skills display 2" className={styles.img2} src="https://user-images.githubusercontent.com/53489910/83068789-7465f480-a02e-11ea-96c2-2aa61133b568.png" />
+                        <img alt="Skills display 3" className={styles.img3} src="https://user-images.githubusercontent.com/53489910/83068982-c9096f80-a02e-11ea-9698-d1517640bf68.png" />
                     </div>
 
                     <p className={styles.skillsText}>*10 corresponds to my highest level of mastery</p>
